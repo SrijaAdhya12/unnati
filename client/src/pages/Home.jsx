80f508ef930c334b8e6169084092bd180becab5b
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-03-30 17:18:53 +0530

    added hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import HeroImg from '../assets/hero.png'
 
 const Home = () => {
     return (
-			<main className="mx-[400px]">
-				<section className="text-black">
-					<div>
-						<h6 className="">Unnati</h6>
-					</div>
-					<div></div>
-				</section>
-			</main>
-		);
+		<main className="mx-[400px]">
+			<section className="text-black font-roboto my-[100px] flex space-x-[190px]">
+				<div className="my-[80px]">
+					<h6 className="font-bold text-[65px]">Unnati</h6>
+					<br />
+					<h6 className="text-[40px] font-regular">-Rise, Empower, Thrive</h6>
+				</div>
+				<div>
+					<img src={HeroImg} alt="" />
+				</div>
+			</section>
+		</main>
+	)
 }
 
 export default Home
