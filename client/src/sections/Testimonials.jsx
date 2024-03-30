ff52501e470f2cb9b8169dcb21fad07a3c403e6d
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-03-31 01:39:21 +0530

    feat: added carousal

--- client/src/sections/Testimonials.jsx
+++ client/src/sections/Testimonials.jsx
@@ -1,7 +1,16 @@
+import { Carousel } from "../components"
+
 const Testimonials = () => {
 	return (
 		<section id="testimonials" className="container mx-auto mt-32 px-32">
-			Testimonials
+			<div>
+			<div>
+				<h1 className="font-roboto text-4xl font-bold mb-10">Testimonials</h1>
+			</div>
+				<div>
+					<Carousel/>
+				</div>
+			</div>
 		</section>
 	)
 }
