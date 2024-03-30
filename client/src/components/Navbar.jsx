b451884c6bd97c2d5fbe208595547a8a4d334f9d
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-03-30 21:09:26 +0530

    completed features section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
+	return (
 		<div className="container mx-[400px]">
 			<div className="my-[60px] font-roboto flex space-x-[600px]">
 				<h5 className="text-black font-extrabold text-3xl">Unnati</h5>
-				<div className='flex space-x-20'>
-					<h6>Features</h6>
-					<h6>Donate</h6>
-                  <h6>Sign up</h6>
-                  <h6>Profile</h6>
+				<div className="flex space-x-20">
+					<a href="">
+						<h6>Features</h6>
+					</a>
+					<a href="">
+						<h6>Donate</h6>
+					</a>
+					<a href="">
+						<h6>Sign up</h6>
+					</a>
+					<a href="">
+						<h6>Profile</h6>
+					</a>
 				</div>
 			</div>
-      </div>
-     
-	);
+		</div>
+	)
 }
 
 export default Navbar
