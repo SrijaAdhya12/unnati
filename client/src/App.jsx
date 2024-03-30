3c5ecfc90e02fd1bffb07105f581403dcdcb1065
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-03-30 16:46:51 +0530

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return <>
+		<Navbar />
+		<Home/>
+	</>
 };
 
 export default App;
